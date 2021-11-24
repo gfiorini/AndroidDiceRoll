5de34e64f1023a373c614331b4c8bc17e42598be
gfiorini <gianluca.fiorini@gmail.com>
2021-11-24 23:06:19 +0100

    Roll image

--- app/src/main/java/it/gianlucafiorini/diceroller/MainActivity.kt
+++ app/src/main/java/it/gianlucafiorini/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package it.gianlucafiorini.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import java.util.*
 
@@ -18,8 +19,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun doRoll() {
-        val view : TextView = findViewById(R.id.roll_text)
+        val diceImage: ImageView = findViewById(R.id.dice_view)
         val diceValue = Random().nextInt(6) + 1
-        view.text = diceValue.toString()
+        val drawableResource = when (diceValue) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
