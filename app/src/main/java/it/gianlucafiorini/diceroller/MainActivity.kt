d40a491e5985eff8ce569aa8af63244b9c04e301
gfiorini <gianluca.fiorini@gmail.com>
2021-11-24 22:34:30 +0100

    Toast!

--- app/src/main/java/it/gianlucafiorini/diceroller/MainActivity.kt
+++ app/src/main/java/it/gianlucafiorini/diceroller/MainActivity.kt
@@ -2,10 +2,21 @@ package it.gianlucafiorini.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollbutton : Button = findViewById(R.id.roll_button);
+        rollbutton.text = "Let's go!!"
+        rollbutton.setOnClickListener {
+            val t : Toast = Toast.makeText(this, "Urrà!", Toast.LENGTH_SHORT)
+            t.show()
+        }
+        
+
     }
 }
